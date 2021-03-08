09be9c23c412f9c856f79d887ad3c0cbc4d9f905
M.boneh <maryamboneh@gmail.com>
2021-03-08 23:10:50 +0330

    insert data (add contact)

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class phonebook(QWidget):
         super(phonebook, self).__init__()
         self.load_ui()
         self.conectDB()
+        self.ui.add_btn.clicked.connect(openAddDg)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -23,25 +24,63 @@ class phonebook(QWidget):
         ui_file.close()
 
     def conectDB(self):
-        conn = sqlite3.connect('phonebook/contacts.db')
+        conn = sqlite3.connect('contacts.db')
         c = conn.cursor()
-        c.execute('SELECT * FROM contacts')
+        c.execute('SELECT * FROM contacts WHERE deleted = 0')
         c2 = conn.cursor()
-        c2.execute('SELECT * FROM contacts')
+        c2.execute('SELECT * FROM contacts WHERE deleted = 0')
 
         self.allSQLRows = c.fetchall()
         self.ui.tableWidget.setRowCount(len(self.allSQLRows))
-        self.ui.tableWidget.setColumnCount(4)
+        self.ui.tableWidget.setColumnCount(3)
 
         row = 0
         while True:
             sqlRow = c2.fetchone()
             if sqlRow == None:
                 break
-            for col in range(0, 4):
+            for col in range(0, 3):
                 self.ui.tableWidget.setItem(
                     row, col, QTableWidgetItem(str(sqlRow[col])))
             row += 1
+
+
+def openAddDg():
+    global addClass
+    addClass = AddContact()
+    addClass.load_ui()
+
+
+class AddContact(QWidget):
+    def __init__(self):
+        super(AddContact, self).__init__()
+        self.load_ui()
+        self.conectDB()
+        self.ui.insert_btn.clicked.connect(self.InsertRow)
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "add-contact.ui")
+        print(path)
+        dg_ui_file = QFile(path)
+        dg_ui_file.open(QFile.ReadOnly)
+        self.ui = loader.load(dg_ui_file, self)
+        dg_ui_file.close()
+
+    def conectDB(self):
+        self.conn = sqlite3.connect('contacts.db')
+
+    def InsertRow(self):
+        name = self.ui.name_txt.toPlainText()
+        phonenumber = self.ui.phone_txt.toPlainText()
+        
+        with self.conn:
+            cur = self.conn.cursor()
+            cur.execute("INSERT INTO contacts (name, phonenumber, deleted)"
+                        "VALUES('%s', '%s', '%s')" % (''.join(name),''.join(phonenumber), 0))
+
+            QMessageBox.about(self, 'Connection', 'Data Inserted Successfully!!')
+            self.close()
 
 
 if __name__ == "__main__":
