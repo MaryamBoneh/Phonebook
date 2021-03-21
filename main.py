7a3bf50f5bc15510713177406c1ed7f478d1cd80
maryamboneh <maryamboneh@gmail.com>
2021-03-22 01:13:38 +0430

    update contact

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class phonebook(QWidget):
         self.conectDB()
         self.ui.add_btn.clicked.connect(self.openAddDg)
         self.ui.del_btn.clicked.connect(self.deleteContact)
+        self.ui.edit_btn.clicked.connect(self.editContact)
         self.ui.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
 
     def conectDB(self):
@@ -46,18 +47,18 @@ class phonebook(QWidget):
     def openAddDg(self):
         self.addClass = AddContact()
 
-    def deleteContact(self, row):
+    def editContact(self):
+        self.editClass = EditContact(
+            self.allSQLRows[self.ui.tableWidget.currentIndex().row()][1])
+
+    def deleteContact(self):
 
         row = self.ui.tableWidget.currentIndex().row()
         if row < 0:
             return
 
         messageBox = QMessageBox.warning(
-            self,
-            "Warning!",
-            "Do you want to remove the selected contact?",
-            QMessageBox.Ok | QMessageBox.Cancel,
-        )
+            self, "Warning!", "Do you want to remove the selected contact?", QMessageBox.Ok | QMessageBox.Cancel,)
 
         if messageBox == QMessageBox.Ok:
             sql = 'DELETE FROM contacts WHERE phonenumber=?'
@@ -88,6 +89,33 @@ class AddContact(QWidget):
             cur = self.conn.cursor()
             cur.execute("INSERT INTO contacts (name, phonenumber, deleted)"
                         "VALUES('%s', '%s', '%s')" % (''.join(name), ''.join(phonenumber), 0))
+
+            self.ui.close()
+            self.close()
+
+
+class EditContact(QWidget):
+    def __init__(self, pn):
+        super(EditContact, self).__init__()
+        loader = QUiLoader()
+        self.ui = loader.load("add-contact.ui")
+        self.ui.show()
+        self.oldpn = pn
+        self.conectDB()
+        self.ui.insert_btn.clicked.connect(self.EditRow)
+        self.ui.cancle_btn.clicked.connect(self.ui.close)
+
+    def conectDB(self):
+        self.conn = sqlite3.connect('contacts.db')
+
+    def EditRow(self):
+        name = self.ui.name_txt.toPlainText()
+        phonenumber = self.ui.phone_txt.toPlainText()
+
+        with self.conn:
+            cur = self.conn.cursor()
+            cur.execute("UPDATE contacts SET name = '%s', phonenumber = '%s' WHERE phonenumber = '%s'" %
+                        (''.join(name), ''.join(phonenumber), self.oldpn))
 
             self.ui.close()
             self.close()
